51b940f383f5572fda56303d9de90652525ea358
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-16 19:38:39 -0400

    bounce and fade in animation

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-TweenMax.to('.circle', 20, {
-  backgroundColor: '#ff0000',
+// ======ball animation function ============
+
+// TweenMax.to('.circle', 20, {
+//   backgroundColor: '#ff0000',
+//   x: 400,
+//   ease: Elastic.easeOut,
+// });
+
+// =================================================
+
+// =========logo animation ====================
+
+// gsap.to('.logo', { duration: 2, x: 300 });
+
+gsap.to('.logo', {
+  duration: 3,
   x: 400,
-  ease: Elastic.easeOut,
+  opacity: 100,
+
+  backgroundColor: '#124563',
+  borderRadius: '10%',
+  border: '5px solid white',
+  padding: '10px',
+  ease: 'bounce
+  ',
 });
