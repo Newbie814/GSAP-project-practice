75ac4b08fb1bedbfd4cf5715bc51c8331ed5b759
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-16 21:07:20 -0400

    multiple objects and animations

--- app.js
+++ app.js
@@ -34,9 +34,31 @@
 //   ease: 'back',
 // });
 
-gsap.from('.circle', {
+// gsap.from('.circle', {
+//   duration: 2,
+//   opacity: 0,
+//   y: 'random(-400, 400)',
+//   stagger: 0.25,
+// });
+let tl = gsap.timeline({ repeat: 2 });
+tl.from('.logo', {
   duration: 2,
   opacity: 0,
-  y: 'random(-400, 400)',
-  stagger: 0.25,
+  scale: 0.1,
+  ease: 'back',
 });
+tl.to('.logo', {
+  duration: 1.5,
+  rotation: 360,
+});
+tl.from(
+  '.circle',
+  {
+    duration: 1,
+    opacity: 0,
+    delay: 1.5,
+    y: 150,
+    stagger: 0.25,
+  },
+  '+=1'
+);
